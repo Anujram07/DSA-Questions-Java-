ae8efa4de0e28752ab442a04adae5518ab3ef034
Anujram07 <anujramraika12@gmail.com>
2025-05-05 18:42:31 +0530

    DSA

---
+++ array_1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class array_1{
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+               System.out.println("Enter size of Array:");
+               int n=in.nextInt();
+               int[] arr=new int[n];
+               System.out.println("enter value of array");
+               for(int i=0;i<n;i++)
+               {
+                arr[i]=in.nextInt();
+               }
+
+
+              int  max = 0;
+            for(int i = 0; i<=arr.length-1;i++){
+                if(arr[i]>arr[i+1]){
+                    max=arr[i];
+                }
+
+            else{
+                max=arr[i+1];
+            }
+       }
+            System.out.println("maximum:-"+max);
+    }
+}
