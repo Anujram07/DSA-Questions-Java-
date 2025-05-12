c88e18c5f6705e6ecc6dcd95c5a9a8f6ca09404b
Anujram07 <anujramraika12@gmail.com>
2025-05-12 09:46:51 +0530

    array

---
+++ Array_6.java
@@ -0,0 +1,19 @@
+//intersection of two Array
+
+public class Array_6 {
+    public static void main(String[] args) {
+        int[] arr1={1,3};
+        int[] arr2={3,4};
+        int[] arr3= new int[arr1.length + arr2.length];
+        
+        for(int i = 0; i<arr1.length;i++){
+            if(arr1[i]==arr2[i]){
+             
+            }
+        }
+
+        for(int i = 0;i<arr3.length; i++){
+             System.out.println(arr3[i]);
+        }
+    }
+}
---
+++ array_7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class array_7 {
+    public static void main(String[] args) {
+         Scanner in  = new Scanner(System.in);
+       System.out.println("Enter size of an array:-");
+       int n = in.nextInt();
+       int[] arr = new int[n];
+       System.out.println("Enter elements of an Array:-");
+       for (int i = 0; i < n; i++) {
+           arr[i]=in.nextInt();
+       }
+       int last = arr[n-1];
+       for( int i=n-1;i>0;i--){
+        arr[i] = arr[i-1];
+       }
+       arr[0] =last;
+
+       System.out.println("Rotated Array:-");
+      for (int num : arr) {
+            System.out.print(num + " ");
+       }
+    }
+}
