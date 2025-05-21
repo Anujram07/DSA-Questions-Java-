5aa5fc657e4d8b954ed024c9f5c84769b324bc63
Anujram07 <anujramraika12@gmail.com>
2025-05-21 12:22:00 +0530

    array

--- Array_5.java
+++ Array_5.java
@@ -20,9 +20,4 @@ public class Array_5 {
         for ( int i=0;i<arr1.length+arr2.length;i++)
         System.out.println(arr3[i]);
     }
-
-
-        //Intersection of Two Array 
-
-        
 }
--- Array_6.java
+++ Array_6.java
@@ -1,19 +1,34 @@
 //intersection of two Array
-
-public class Array_6 {
+ public class Array_6 {
     public static void main(String[] args) {
-        int[] arr1={1,3};
-        int[] arr2={3,4};
-        int[] arr3= new int[arr1.length + arr2.length];
-        
-        for(int i = 0; i<arr1.length;i++){
-            if(arr1[i]==arr2[i]){
-             
+        int[] arr1 = {1, 3};
+        int[] arr2 = {3, 4};
+
+        // Result array, maximum size = min(arr1.length, arr2.length)
+        int[] intersection = new int[Math.min(arr1.length, arr2.length)];
+        int k = 0; // index for intersection array
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    // Check if already added (to avoid duplicates)
+                    boolean Added = false;
+                    for (int m = 0; m < k; m++) {
+                        if (intersection[m] == arr1[i]) {
+                            Added = true;
+                            break;
+                        }
+                    }
+                    if (!Added) {
+                        intersection[k++] = arr1[i];
+                    }
+                }
             }
         }
 
-        for(int i = 0;i<arr3.length; i++){
-             System.out.println(arr3[i]);
+        // Print the intersection elements
+        for (int i = 0; i < k; i++) {
+            System.out.println(intersection[i]);
         }
     }
 }
